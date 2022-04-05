6bd4267dbd984ad7685d951525b1245075070d1f
Ivan Sprut <i.sprutfromua@gmail.com>
2022-04-05 18:52:22 +0300

    COMM-0005: Comment actions

---
+++ src/lib/CommentActions/CommentAction.svelte
@@ -0,0 +1,5 @@
+<button
+	class="flex gap-x-2 transition-colors items-center focus:outline-dotted-purple-600 {$$props.class}"
+>
+	<slot />
+</button>
---
+++ src/lib/CommentActions/CommentActions.svelte
@@ -0,0 +1,26 @@
+<script>
+  import IconReply from "$lib/icons/IconReply.svelte";
+  import IconEdit from "$lib/icons/IconEdit.svelte";
+  import IconDelete from "$lib/icons/IconDelete.svelte";
+  import CommentAction from "./CommentAction.svelte";
+</script>
+
+<div class="flex gap-x-4 items-center">
+  <CommentAction class="text-softRed hover:text-paleRed">
+    <IconDelete />
+
+    <span class="font-medium leading-6">Delete</span>
+  </CommentAction>
+
+  <CommentAction class="text-moderateBlue hover:text-lightGrayishBlue">
+    <IconEdit />
+
+    <span class="font-medium leading-6">Edit</span>
+  </CommentAction>
+
+  <CommentAction class="text-moderateBlue hover:text-lightGrayishBlue">
+    <IconReply />
+
+    <span class="font-medium leading-6">Reply</span>
+  </CommentAction>
+</div>
