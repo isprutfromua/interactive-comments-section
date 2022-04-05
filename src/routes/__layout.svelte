2b907f8546d6219074f7686d46417a3d1edb4f00
Ivan Sprut <i.sprutfromua@gmail.com>
2022-04-05 18:51:13 +0300

    COMM-0001: Base layout setup, data upload

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,7 @@
-<slot />
+<div
+  class="bg-veryLightGray min-h-screen w-full antialiased lg:p-16 <lg:py-8 <lg:px-4">
+  <slot />
+</div>
 
 <style windi:preflights:global windi:safelist:global>
 </style>
