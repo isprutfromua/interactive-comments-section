05a5c7d27039d15b8875b23de80879dc96ab5ed3
Ivan Sprut <i.sprutfromua@gmail.com>
2022-04-05 18:52:46 +0300

    COMM-0007: Comment form

---
+++ src/lib/CommentForm/CommentForm.svelte
@@ -0,0 +1,22 @@
+<script>
+  import CommentInput from "./CommentInput.svelte";
+  import Button from "$lib/components/Button.svelte";
+  import UserAvatar from "$lib/components/UserAvatar.svelte";
+  import ComponentsWrapper from "$lib/components/ComponentsWrapper.svelte";
+</script>
+
+<ComponentsWrapper
+  class="gap-y-2 gap-x-2 grid-cols-[max-content,1fr,max-content]">
+  <div class="h-10 w-10 <lg:row-start-2">
+    <UserAvatar />
+  </div>
+  <div class="<lg:col-span-full">
+    <CommentInput />
+  </div>
+  <div class="<lg:col-start-3">
+    <Button
+      class="bg-moderateBlue hover:bg-lightGrayishBlue focus:bg-lightGrayishBlue ">
+      send
+    </Button>
+  </div>
+</ComponentsWrapper>
---
+++ src/lib/CommentForm/CommentInput.svelte
@@ -0,0 +1,3 @@
+<textarea
+  class="border  border-lightGray rounded-main text-darkBlue w-full py-4 px-6 leading-6  resize-none focus:border-moderateBlue focus:outline-none"
+  placeholder="Add a comment…" />
