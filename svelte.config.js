fa4da1401e93aca0256ac3b1aa61f2535a1ac21f
Ivan Sprut <i.sprutfromua@gmail.com>
2022-04-05 22:32:53 +0300

    COMM-0012: aliases setup

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { windi } from 'svelte-windicss-preprocess';
 
+import path from 'path';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -10,7 +12,16 @@ const config = {
 
 	kit: {
 		adapter: adapter(),
-	},
+		vite: {
+			resolve: {
+				alias: {
+					$stores: path.resolve('./src/stores'),
+					$types: path.resolve('./src/types'),
+					$icons: path.resolve('./src/lib/icons')
+				}
+			}
+		}
+	}
 };
 
 export default config;
